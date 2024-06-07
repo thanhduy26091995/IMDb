f0712469d536dddfd5bfe0d8e1001342e068ccad
Duy Bui <duyb@joblogic.com>
2024-06-07 18:46:40 +0700

    Update NetworkModule

--- shared/src/commonMain/kotlin/di/NetworkModule.kt
+++ shared/src/commonMain/kotlin/di/NetworkModule.kt
@@ -6,10 +6,45 @@ import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 import presentation.home.HomeViewModel
 
-fun commonModule() = module {
-    single<IMoviesRepository> {
-        MoviesRepositoryImpl(get())
-    }
+fun networkModule(enableLog: Boolean) = module {
+    single {
+        HttpClient {
+            expectSuccess = true
+            addDefaultResponseValidation()
 
-    singleOf(::HomeViewModel)
+            defaultRequest {
+                url {
+                    protocol = URLProtocol.HTTPS
+                    host = "imdb-top-100-movies.p.rapidapi.com"
+                    headers {
+                        header(
+                            "X-RapidAPI-Key",
+                            ""
+                        )
+                        header("X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com")
+                    }
+                }
+            }
+
+            if (enableLog) {
+                install(Logging) {
+                    level = LogLevel.ALL
+                    logger = object : Logger {
+                        override fun log(message: String) {
+                            // TODO Save or print log here
+                        }
+                    }
+                }
+            }
+
+            install(ContentNegotiation) {
+                json(
+                    Json {
+                        ignoreUnknownKeys = true
+                        isLenient = true
+                    }
+                )
+            }
+        }
+    }
 }
